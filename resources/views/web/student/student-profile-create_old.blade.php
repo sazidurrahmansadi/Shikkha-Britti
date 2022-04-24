51bbad817c8abeb4fb68c000e0ae7b3fde7377a2
prodhan482 <96787662+prodhan482@users.noreply.github.com>
2022-04-24 14:54:29 +0600

    Updated Student ....
    Updated the profile create form, edit profile form....(Student)

--- resources/views/web/student/student-profile-create_old.blade.php
+++ resources/views/web/student/student-profile-create_old.blade.php
@@ -5,219 +5,6 @@
 
 @endsection
 
-<style>
-    body {
-        font-family: Montserrat, "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
-        margin: 0;
-        display: grid;
-    }
-
-    /* Global Stylings */
-    label {
-        display: block;
-        margin-bottom: 0.5rem;
-    }
-
-    input {
-        display: block;
-        width: 100%;
-        padding: 0.75rem;
-        border: 1px solid #ccc;
-        border-radius: 0.25rem;
-    }
-
-    .width-50 {
-        width: 50%;
-    }
-
-    .ml-auto {
-        margin-left: auto;
-    }
-
-    .text-center {
-        text-align: center;
-    }
-
-    /* Progressbar */
-    .progressbar {
-        position: relative;
-        display: flex;
-        justify-content: space-between;
-        counter-reset: step;
-        margin: 2rem 0 4rem;
-    }
-
-    .progressbar::before,
-    .progress {
-        content: "";
-        position: absolute;
-        top: 50%;
-        transform: translateY(-50%);
-        height: 4px;
-        width: 100%;
-        background-color: #dcdcdc;
-        z-index: -1;
-    }
-
-    .progress {
-        background-color: var(--primary-color);
-        width: 0%;
-        transition: 0.3s;
-    }
-
-    .progress-step {
-        width: 2.1875rem;
-        height: 2.1875rem;
-        background-color: #dcdcdc;
-        border-radius: 50%;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
-
-    .progress-step::before {
-        counter-increment: step;
-        content: counter(step);
-    }
-
-    .progress-step::after {
-        content: attr(data-title);
-        position: absolute;
-        top: calc(100% + 0.5rem);
-        font-size: 0.85rem;
-        color: #666;
-    }
-
-    .progress-step-active {
-        background-color: var(--primary-color);
-        color: #f3f3f3;
-    }
-
-    /* Form */
-    .form {
-        width: clamp(320px, 30%, 430px);
-        margin: 0 auto;
-        border: 1px solid #ccc;
-        border-radius: 0.35rem;
-        padding: 1.5rem;
-    }
-
-    .form-step {
-        display: none;
-        transform-origin: top;
-        animation: animate 0.5s;
-    }
-
-    .form-step-active {
-        display: block;
-    }
-
-    .input-group {
-        margin: 2rem 0;
-    }
-
-    @keyframes animate {
-        from {
-            transform: scale(1, 0);
-            opacity: 0;
-        }
-
-        to {
-            transform: scale(1, 1);
-            opacity: 1;
-        }
-    }
-
-    /* Button */
-    .btns-group {
-        display: grid;
-        grid-template-columns: repeat(2, 1fr);
-        gap: 25rem;
-    }
-
-    .button2 {
-  display: inline-block;
-  border-radius: 4px;
-  background-color: #f4511e;
-  border: none;
-  color: #FFFFFF;
-  text-align: center;
-  font-size: 15px;
-  padding: 12px;
-  width: 100px;
-  transition: all 0.5s;
-  cursor: pointer;
-  margin: 5px;
-}
-
-.button2 span {
-  cursor: pointer;
-  display: inline-block;
-  position: relative;
-  transition: 0.5s;
-}
-
-.button2 span:after {
-  content: '\21A0';
-  position: absolute;
-  opacity: 0;
-  top: 0;
-  right: -20px;
-  transition: 0.5s;
-}
-
-.button2:hover span {
-  padding-right: 25px;
-}
-
-.button2:hover span:after {
-  opacity: 1;
-  right: 0;
-}
-
-.button1 {
-  display: inline-block;
-  border-radius: 4px;
-  background-color: #f4511e;
-  border: none;
-  color: #FFFFFF;
-  text-align: center;
-  font-size: 15px;
-  padding: 12px;
-  width: 100px;
-  transition: all 0.5s;
-  cursor: pointer;
-  margin: 5px;
-}
-
-.button1 span {
-  cursor: pointer;
-  display: inline-block;
-  position: relative;
-  transition: 0.5s;
-}
-
-.button1 span:after {
-  content: '\219E';
-  position: absolute;
-  opacity: 0;
-  top: 0;
-  left: -20px;
-  transition: 0.5s;
-}
-
-.button1:hover span {
-  padding-left: 25px;
-}
-
-.button1:hover span:after {
-  opacity: 1;
- left: 0;
-}
-
-</style>
-
-
 @section('content')
 
     <section class="page-title title-bg10">
@@ -253,25 +40,14 @@
                     @include('include.messages')
 
                     <div class="account-details">
-                        <form class="basic-info needs-validation" action="{{ route('store_student_information') }}"
-                        method="POST">
                         <h4 class="text-center text-danger">Please provide the below information to Create your profile</h4>
                         <hr>
-                            <!-- Progress bar -->
-                            <!-- <div class="progressbar">
-                                <div class="progress" id="progress"></div>
 
-                                <div class="progress-step progress-step-active" data-title="BasicInfo"></div>
-                                <div class="progress-step" data-title="Contact"></div>
-                                <div class="progress-step" data-title="ID"></div>
-                                <div class="progress-step" data-title="Password"></div>
-                            </div> -->
-
-                            <!-- Steps -->
+                        <h3>Basic Information</h3>
+                        <form class="basic-info needs-validation" action="{{ route('store_student_information') }}"
+                            method="POST">
                             @csrf
-                            <div class="form-step form-step-active">
-                                <h3>Basic Information</h3>
-                                <input type="hidden" name="user_id" value="{{ auth()->user()->id }}">
+                            <input type="hidden" name="user_id" value="{{ auth()->user()->id }}">
 
                             <div class="row">
                                 <div class="col-md-6">
@@ -359,12 +135,9 @@
                                     </div>
                                 </div>
                             </div>
-                                <div class="">
-                                    <a class="button2 btn-next width-50 ml-auto" style="vertical-align:middle"><span>Next </span></a>
-                                </div>
-                            </div>
-                            <div class="form-step">
-                                <h3>Current Academic information</h3>
+
+                            <br>
+                            <h3>Current Academic information</h3>
                             <div class="row dynm_field">
                                 <div class="col-md-6">
                                     <div class="form-group">
@@ -554,13 +327,11 @@
                             </div>
                             <a href="javascript:void(0);" class="add_button" title="Add Achievements"><i
                                     class="bx bx-plus">Add Achievements</i></a>
-                                <div class="btns-group">
-                                    <a class="button1 btn-prev width-50 ml-auto" style="vertical-align:middle"><span>Previous </span></a>
-                                    <a class="button2 btn-next width-50 ml-auto" style="vertical-align:middle"><span>Next </span></a>
-                                </div>
-                            </div>
-                            <div class="form-step">
-                                <h3>Reference Details</h3>
+
+
+                            <br>
+                            <br>
+                            <h3>Reference Details</h3>
                             <div class="row">
                                 <div class="col-md-6">
                                     <div class="form-group">
@@ -631,13 +402,9 @@
                                 </div>
                             </div>
 
-                                <div class="btns-group">
-                                    <a class="button1 btn-prev width-50 ml-auto" style="vertical-align:middle"><span>Previous </span></a>
-                                    <a class="button2 btn-next width-50 ml-auto" style="vertical-align:middle"><span>Next </span></a>
-                                </div>
-                            </div>
-                            <div class="form-step">
-                                <h3>Present Address</h3>
+                            <br>
+
+                            <h3>Present Address</h3>
                             <div class="row">
                                 <div class="col-md-6">
                                     <div class="form-group">
@@ -738,12 +505,7 @@
 
                                 <div class="col-md-12">
                                     {{-- <button type="submit" class="account-btn">Edit</button> --}}
-                                    {{-- <button type="submit" class="account-btn">Save</button> --}}
-                                </div>
-                            </div>
-                                <div class="btns-group">
-                                    <a class="button1 btn-prev width-50 ml-auto" style="vertical-align:middle"><span>Previous </span></a>
-                                    <button class="button2" style="vertical-align:middle"><span>Update</span></button>
+                                    <button type="submit" class="account-btn">Save</button>
                                 </div>
                             </div>
                         </form>
@@ -842,53 +604,5 @@
             });
         });
     </script>
-<script>
-    const prevBtns = document.querySelectorAll(".btn-prev");
-    const nextBtns = document.querySelectorAll(".btn-next");
-    const progress = document.getElementById("progress");
-    const formSteps = document.querySelectorAll(".form-step");
-    const progressSteps = document.querySelectorAll(".progress-step");
 
-    let formStepsNum = 0;
-
-    nextBtns.forEach((btn) => {
-        btn.addEventListener("click", () => {
-            formStepsNum++;
-            updateFormSteps();
-            updateProgressbar();
-        });
-    });
-
-    prevBtns.forEach((btn) => {
-        btn.addEventListener("click", () => {
-            formStepsNum--;
-            updateFormSteps();
-            updateProgressbar();
-        });
-    });
-
-    function updateFormSteps() {
-        formSteps.forEach((formStep) => {
-            formStep.classList.contains("form-step-active") &&
-                formStep.classList.remove("form-step-active");
-        });
-
-        formSteps[formStepsNum].classList.add("form-step-active");
-    }
-
-    function updateProgressbar() {
-        progressSteps.forEach((progressStep, idx) => {
-            if (idx < formStepsNum + 1) {
-                progressStep.classList.add("progress-step-active");
-            } else {
-                progressStep.classList.remove("progress-step-active");
-            }
-        });
-
-        const progressActive = document.querySelectorAll(".progress-step-active");
-
-        progress.style.width =
-            ((progressActive.length - 1) / (progressSteps.length - 1)) * 100 + "%";
-    }
-</script>
 @endsection
